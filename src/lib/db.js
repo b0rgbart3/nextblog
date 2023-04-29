96e02de16e03c3b3dbb7925800b4ff87ecacc378
Bart Dority <b0rgBart3@gmail.com>
2023-04-28 18:24:03 -0700

    removing port

--- src/lib/db.js
+++ src/lib/db.js
@@ -1,8 +1,8 @@
 import mysql from 'serverless-mysql';
+
 const db = mysql({
   config: {
     host:      process.env.DB_HOST,
-    port:      process.env.MYSQL_PORT,
     database:  process.env.DB,
     user:      process.env.DB_USER,
     password:  process.env.DB_USER_PASSWORD
